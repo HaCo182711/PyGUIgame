7292704f41a453b180a5a858d25bd29a444a2ccd
HaCo182711 <100158386+HaCo182711@users.noreply.github.com>
2022-03-01 10:08:31 +0000

    added upon the font system

--- PyGUIgame.py
+++ PyGUIgame.py
@@ -7,188 +7,205 @@ HOLLOW = True
 
 WHITE = (255,255,255)
 BLACK = (0,0,0)
+LIGHT_GRAY = (200,200,200)
+GRAY = (120,120,120)
+DARK_GRAY = (50,50,50)
+
 RED = (255,0,0)
 GREEN = (0,255,0)
 BLUE = (0,0,255)
+YELLOW = (255,255,0)
 
 keycodes = {}
 
 keycodes.update({
-  27:"Escape",
-  1073741906:"Up",
-  1073741905:"Down",
-  1073741904:"Left",
-  1073741903:"Right",
-  1073742049:"LShift",
-  1073742053:"RShift",
-  1073742048:"LCtrl",
-  1073742052:"RCtrl",
-  1073742050:"Alt",
-  32:"Space",
-  13:"Enter",
-  8:"Backspace",
-  9:"Tab"
-  })
+	27:"Escape",
+	1073741906:"Up",
+	1073741905:"Down",
+	1073741904:"Left",
+	1073741903:"Right",
+	1073742049:"LShift",
+	1073742053:"RShift",
+	1073742048:"LCtrl",
+	1073742052:"RCtrl",
+	1073742050:"Alt",
+	32:"Space",
+	13:"Enter",
+	8:"Backspace",
+	9:"Tab"
+	})
 for i in range(49,58):
-  keycodes[i] = str(i-48)
+	keycodes[i] = str(i-48)
 keycodes[48] = "0"
 for i in range(97,123):
-  keycodes[i] = chr(i)
+	keycodes[i] = chr(i)
 
 
 
 def init(width, height, name, FPS=60):
-  return pggWindow(width, height, name, FPS)
+	return pggWindow(width, height, name, FPS)
+
+def get_fonts():
+	return pygame.font.get_fonts() + ["freesansbold", "arial", "aerial"]
+def get_default_font():
+	return pygame.font.get_default_font().split(".")[0]
 
 class Font:
-  def __init__(self,fontname,size):
-    self.Font = pygame.font.Font(fontname, size)
+	def __init__(self,fontname,size):
+		self.Font = pygame.font.SysFont(fontname, size)
 
 class Text:
-  def __init__(self,text,color=(0,0,0),font=Font("freesansbold.ttf",20)):
-    self.__Text = font.Font.render(text, True, color)
-    self.__width = self.__Text.get_width()
-    self.__height = self.__Text.get_height()
-  def getWidth(self):
-    return self.__width
-  def getHeight(self):
-    return self.__height
-  def getText(self):
-    return self.__Text
+	def __init__(self,text,color=(0,0,0),font=Font(get_default_font(),20)):
+		self.__Text = font.Font.render(text, True, color)
+		self.__width = self.__Text.get_width()
+		self.__height = self.__Text.get_height()
+	def getWidth(self):
+		return self.__width
+	def getHeight(self):
+		return self.__height
+	def getText(self):
+		return self.__Text
 
 class pggWindow:
-  def __init__(self, width, height, name, FPS):
-    self.__width = width
-    self.__height = height
-    self.__name = name
+	def __init__(self, width, height, name, FPS):
+		self.__width = width
+		self.__height = height
+		self.__name = name
 
-    self.__SetFPS = FPS
-    self.__FPS = 0
-    self.__AvFPS = 0
-    self.__Time = 0
-    self.__Frames = 0
+		self.__SetFPS = FPS
+		self.__FPS = 0
+		self.__AvFPS = 0
+		self.__Time = 0
+		self.__Frames = 0
 
-    self.__Clock = pygame.time.Clock()
+		self.__Clock = pygame.time.Clock()
 
-    self.__win = pygame.display.set_mode((self.__width, self.__height))
-    pygame.display.set_caption(self.__name)
+		self.__win = pygame.display.set_mode((self.__width, self.__height))
+		pygame.display.set_caption(self.__name)
 
-    self.__keys = []
-    self.__mouseButtons = []
-    self.__pos = 0,0
+		self.__QUIT = False
+		self.QUIT = False
 
-    self.__mouseDownEvent = False
-    self.__mouseUpEvent = False
-    self.__keyDownsEvent = []
+		self.__keys = []
+		self.__mouseButtons = []
+		self.__pos = 0,0
 
-
-  def getWidth(self):
-    return self.__width
-  def getHeight(self):
-    return self.__height
-  def getName(self):
-    return self.__name
-
-  def getSetFPS(self):
-    return self.__SetFPS
-  def getFPS(self):
-    return round(self.__FPS,2)
-  def getAvFPS(self):
-    return round(self.__AvFPS,2)
-  def getTime(self):
-    return self.__Time
-  def getFrames(self):
-    return self.__Frames
-
-  def isKeyDown(self, keycode):
-    if keycode in self.__keys:
-      return True
-    return False
-  def getKeysDown(self):
-    return self.__keys
-
-  def getMousePos(self):
-    return self.__pos
-  def getMousePressed(self):
-    return self.__mouseButtons
-  def getLeftMouse(self):
-    return self.__mouseButtons[0]
-  def getRightMouse(self):
-    return self.__mouseButtons[1]
-  
-  def MouseDown(self):
-    return self.__mouseDownEvent
-  def MouseUp(self):
-    return self.__mouseUpEvent
-  def getKeysPressed(self):
-    return self.__keysDownEvent
-  def isKeyPressed(self, keycode):
-    if keycode in self.__keysDownEvent:
-      return True
-    return False
+		self.__mouseDownEvent = False
+		self.__mouseUpEvent = False
+		self.__keyDownsEvent = []
 
 
-  def update(self):
+	def getWidth(self):
+		return self.__width
+	def getHeight(self):
+		return self.__height
+	def getName(self):
+		return self.__name
 
-    pygame.display.update()
-    self.__Clock.tick(self.__SetFPS)
-    self.__FPS = self.__Clock.get_fps()
-    self.__Time = pygame.time.get_ticks()/1000
-    self.__Frames += 1
-    if self.__Time % 1 > 1 - 1/self.__SetFPS:
-      self.__AvFPS = self.__FPS
+	def getSetFPS(self):
+		return self.__SetFPS
+	def getFPS(self):
+		return round(self.__FPS,2)
+	def getAvFPS(self):
+		return round(self.__AvFPS,2)
+	def getTime(self):
+		return self.__Time
+	def getFrames(self):
+		return self.__Frames
 
-    self.__win.fill((0,0,0))
+	def isKeyDown(self, keycode):
+		if keycode in self.__keys:
+			return True
+		return False
+	def getKeysDown(self):
+		return self.__keys
 
-    self.__pos = pygame.mouse.get_pos()
-    
-    self.__mouseButtons = [pygame.mouse.get_pressed(num_buttons=3)]
+	def getMousePos(self):
+		return self.__pos
+	def getMousePressed(self):
+		return self.__mouseButtons
+	def getLeftMouse(self):
+		return self.__mouseButtons[0]
+	def getRightMouse(self):
+		return self.__mouseButtons[1]
+	
+	def MouseDown(self):
+		return self.__mouseDownEvent
+	def MouseUp(self):
+		return self.__mouseUpEvent
+	def getKeysPressed(self):
+		return self.__keysDownEvent
+	def isKeyPressed(self, keycode):
+		if keycode in self.__keysDownEvent:
+			return True
+		return False
 
-    self.__keysDownEvent = []
-    for event in pygame.event.get():
-      if event.type == pygame.KEYDOWN:
-        if event.key in keycodes:
-          self.__keys.append(keycodes[event.key])
-          self.__keysDownEvent.append(keycodes[event.key])
-        else:
-          self.__keys.append(event.key)
-          self.__keysDownEvent.append(event.key)
-      if event.type == pygame.KEYUP:
-        if event.key in keycodes:
-          if keycodes[event.key] in self.__keys:
-            self.__keys.remove(keycodes[event.key])
-        elif event.key in self.__keys:
-          self.__keys.remove(event.key)
-          
-      self.__mouseDown = False
-      if event.type == pygame.MOUSEBUTTONDOWN:
-        self.__mouseDown = True
-      self.__mouseUp = False
-      if event.type == pygame.MOUSEBUTTONUP:
-        self.__mouseUp = True
 
-  def background(self, color):
-    self.__win.fill(color)
+	def update(self):
 
-  def draw_line(self, color, p1, p2):
-    pygame.draw.line(self.__win, color, p1, p2)
+		pygame.display.update()
+		self.__Clock.tick(self.__SetFPS)
+		self.__FPS = self.__Clock.get_fps()
+		self.__Time = pygame.time.get_ticks()/1000
+		self.__Frames += 1
+		if self.__Time % 1 > 1 - 1/self.__SetFPS:
+			self.__AvFPS = self.__FPS
+
+		self.__win.fill((0,0,0))
+
+		self.__pos = pygame.mouse.get_pos()
+		
+		self.__mouseButtons = [pygame.mouse.get_pressed(num_buttons=3)]
+
+		self.__keysDownEvent = []
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				self.__QUIT = True
+			if event.type == pygame.KEYDOWN:
+				if event.key in keycodes:
+					self.__keys.append(keycodes[event.key])
+					self.__keysDownEvent.append(keycodes[event.key])
+				else:
+					self.__keys.append(event.key)
+					self.__keysDownEvent.append(event.key)
+			if event.type == pygame.KEYUP:
+				if event.key in keycodes:
+					if keycodes[event.key] in self.__keys:
+						self.__keys.remove(keycodes[event.key])
+				elif event.key in self.__keys:
+					self.__keys.remove(event.key)
+					
+			self.__mouseDown = False
+			if event.type == pygame.MOUSEBUTTONDOWN:
+				self.__mouseDown = True
+			self.__mouseUp = False
+			if event.type == pygame.MOUSEBUTTONUP:
+				self.__mouseUp = True
+				
+		self.QUIT = self.__QUIT
+
+	def background(self, color):
+		self.__win.fill(color)
+
+	def draw_line(self, color, p1, p2):
+		pygame.draw.line(self.__win, color, p1, p2)
 	# takes in a point from where to start and the dimensions of the rectangle
-  def draw_rect(self, color, p, dim, filled=False):
-    pygame.draw.rect(self.__win, color, (p, dim), filled)
-  def draw_circle(self, color, p, r, filled=False):
-    pygame.draw.circle(self.__win, color, p, r, filled)
-  def draw_text(self, text, p):
-    self.__win.blit(text.getText(), p)
+	def draw_rect(self, color, p, dim, filled=False):
+		pygame.draw.rect(self.__win, color, (p, dim), filled)
+	def draw_circle(self, color, p, r, filled=False):
+		pygame.draw.circle(self.__win, color, p, r, filled)
+	def draw_text(self, text, p):
+		self.__win.blit(text.getText(), p)
 
 if __name__ == "__main__":
-  print("this is a demo")
-  win = init(512,256,"DemoWindow")
-  
-  run = True
-  while run:
-    win.update()
-    win.background(RED)
-    win.draw_line(WHITE,(10,10),(50,50))
-    win.draw_rect(WHITE, (30,30), (90,90), HOLLOW)
-    win.draw_circle(WHITE, (100,30),10,FILLED)
-    print(win.pos, win.keys, win.mouseButtons)
+	print("this is a demo")
+	win = init(512,256,"DemoWindow")
+	
+	run = True
+	while run:
+		win.update()
+		win.background(RED)
+		win.draw_line(WHITE,(10,10),(50,50))
+		win.draw_rect(WHITE, (30,30), (90,90), HOLLOW)
+		win.draw_circle(WHITE, (100,30),10,FILLED)
+		print(win.pos, win.keys, win.mouseButtons)
--- demo.py
+++ demo.py
@@ -3,34 +3,48 @@ import PyGUIgame as pgg
 # more then 120 isn't accurate nor necessary
 win = pgg.init(512,256, "demo window", FPS=120)
 
+# prints all available fonts
+print("all available fonts:",pgg.get_fonts())
+print("default font:",pgg.get_default_font())
+
 run = True
 while run:
-    win.update()
+		# updates all variables in win (keys,fps,...)
+		win.update()
 
-    win.background(pgg.RED)
+		# fills the background
+		win.background(pgg.GRAY)
 
-    win.draw_line(pgg.WHITE,(350,10),(500,50))
-    win.draw_rect(pgg.WHITE, (350,50), (100,90), pgg.HOLLOW)
-    win.draw_circle(pgg.WHITE, (400,160),10,pgg.FILLED)
+		# draws a line, rect and circle
+		win.draw_line(pgg.WHITE,(350,10),(500,50))
+		win.draw_rect(pgg.WHITE, (350,50), (100,90), pgg.HOLLOW)
+		win.draw_circle(pgg.WHITE, (400,160),10,pgg.FILLED)
 
-    font = pgg.Font("freesansbold.ttf",15)
+		# text, fonts, colors and system variables
+		# makes a font
+		font = pgg.Font("dejavusans",20)
 
-    hello = pgg.Text("hello world!")
-    otherfont = pgg.Text("this is written in another font",font=font)
-    othercolor = pgg.Text("this is written in another color",color=pgg.BLUE)
-    frame = pgg.Text(f"framecounter: {win.getFrames()}",font=font)
-    fps = pgg.Text(f"fps: {win.getFPS()}",font=font)
-    avfps = pgg.Text(f"av fps: {win.getAvFPS()}",font=font)
-    gametime = pgg.Text(f"time: {win.getTime()}",font=font)
-    win.draw_text(hello, (10,10))
-    win.draw_text(otherfont, (10,50))
-    win.draw_text(othercolor, (10,70))
-    win.draw_text(frame, (10,90))
-    win.draw_text(fps, (10,120))
-    win.draw_text(avfps, (10,150))
-    win.draw_text(gametime, (10,190))
+		# makes the text
+		hello = pgg.Text("hello world!",font=font)
+		otherfont = pgg.Text("this is written in the standard font")
 
-    #print(win.getMousePos(), win.getKeysDown(), win.getMousePressed())
+		othercolor = pgg.Text("this is written in another color",color=pgg.BLUE, font=font)
+		frame = pgg.Text(f"framecounter: {win.getFrames()}",font=font)
+	
+		fps = pgg.Text(f"fps: {win.getFPS()}",font=font)
+		avfps = pgg.Text(f"av fps: {win.getAvFPS()}",font=font)
+	
+		gametime = pgg.Text(f"time: {win.getTime()}",font=font)
 
-    if win.isKeyPressed("Escape"):
-      run = False
+		# draws the text
+		win.draw_text(hello, (10,10))
+		win.draw_text(otherfont, (10,50))
+		win.draw_text(othercolor, (10,70))
+		win.draw_text(frame, (10,90))
+		win.draw_text(fps, (10,120))
+		win.draw_text(avfps, (10,150))
+		win.draw_text(gametime, (10,190))
+
+		# is key escape is pressed, the programs stops
+		if win.isKeyPressed("Escape") or win.QUIT:
+			run = False
